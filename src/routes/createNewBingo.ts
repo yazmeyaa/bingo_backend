5fba6677a44a164283974927ab8f6f10fcf9e118
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-11-29 17:21:57 +0300

    Добавлены проверки на наличие и ответ

--- src/routes/createNewBingo.ts
+++ src/routes/createNewBingo.ts
@@ -2,7 +2,9 @@ import redisClient from "config/redis";
 import { Request, Response } from "express";
 
 async function createNewBingo(req: Request<undefined, undefined, { template?: string[] }>, res: Response) {
-    if (!req.body.template) return res.status(200).send({ error: "Нет данных для бинго!" })
+    if (!req.body.template) return res.status(400).send({ error: "Нет данных для бинго!" })
+    const { template } = req.body
+    if (template.length <= 0) return res.status(400).send({ error: "Необходим хотя бы один элемент для бинго" })
     const bingoTemplate = req.body.template
     const keys = await redisClient.keys('*')
     const newData = {
@@ -10,7 +12,7 @@ async function createNewBingo(req: Request<undefined, undefined, { template?: st
         values: bingoTemplate
     }
     redisClient.set(keys.length.toString(), JSON.stringify(newData))
-    res.status(200).send({ message: "Новое бинго создано!" })
+    res.status(200).send({ message: "Новое бинго создано!", bingo: newData })
 }
 
 export default createNewBingo
